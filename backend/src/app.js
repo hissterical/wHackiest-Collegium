4fc1dad653cb73da63133c44164283b27cb65e77
hissterical <68277986+hissterical@users.noreply.github.com>
2024-12-22 04:38:39 +0530

    allow requests from all origins (fix)

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,14 +10,14 @@ const app = express();
 app.use(express.json());
 
 //cors
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*"); 
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//     res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization"); 
-//     next();
-// });
-const cors = require('cors');
-app.use(cors()); 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization"); 
+    next();
+});
+// const cors = require('cors');
+// app.use(cors()); 
 
 // routes
 app.use('/api/users', userRoutes);
